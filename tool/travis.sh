9bfb676f98cf1dad3e9aa059c4004be402b7aa89
Alexandre Roux Tekartik <alex@tekartik.com>
2016-10-07 00:22:52 +0200

    test: add chrome travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -10,5 +10,6 @@ dartanalyzer --fatal-warnings \
   lib/fs_memory.dart \
 
 pub run test -p vm
+pub run test -p chrome
 # pub run test -p content-shell -j 1
 # pub run test -p firefox -j 1 --reporter expanded
