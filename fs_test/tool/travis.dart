c201bf0aa7b967f6d666a8a8f4e928eecffcfab7
alex <alex@tekartik.com>
2019-03-04 08:14:09 +0100

    test: use shell command

---
+++ fs_test/tool/travis.dart
@@ -0,0 +1,17 @@
+import 'package:process_run/shell.dart';
+
+Future<void> main() async {
+  final Shell shell = Shell();
+
+  await shell.run('''
+
+dartanalyzer --fatal-warnings --fatal-infos .
+
+# failing for now 2018-06-30 
+pub run test -p vm,chrome
+
+pub run test -p vm
+pub run build_runner test -- -p vm,chrome
+
+''');
+}
