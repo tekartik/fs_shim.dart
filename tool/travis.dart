5081c1b8d8945eb2456acc2f1df2669c9a6c9138
alex <alex@tekartik.com>
2019-07-15 13:52:32 +0200

    fix: node on SDK 2.5

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,23 +1,18 @@
-import 'dart:io';
-
 import 'package:process_run/shell.dart';
-import 'package:pub_semver/pub_semver.dart';
-
-import 'tool_test.dart';
 
 Future main() async {
   var shell = Shell();
 
-  var dartVersion = parsePlatformVersion(Platform.version);
-  bool oldListInt = dartVersion <= Version(2, 5, 0, pre: 'dev');
+  // var dartVersion = parsePlatformVersion(Platform.version);
+  // bool oldListInt = dartVersion <= Version(2, 5, 0, pre: 'dev');
 
   var packages = [
     'fs',
     'fs_browser',
-    if (oldListInt) 'fs_node',
+    'fs_node',
     'fs_test',
   ];
-  print('dartVersion: $dartVersion, oldListInt:${oldListInt}');
+  // print('dartVersion: $dartVersion, oldListInt:${oldListInt}');
   print('packages: $packages');
   for (var dir in packages) {
     shell = shell.pushd(dir);
