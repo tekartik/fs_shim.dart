e4b939efa56ee77449441e9b3457e901d3d38c06
alex <alex@tekartik.com>
2021-04-01 14:47:16 +0200

    fs_shim v2.0.1+1

--- fs_io/test/fs_io_test.dart
+++ fs_io/test/fs_io_test.dart
@@ -1,18 +1,14 @@
+@TestOn('vm')
 import 'package:tekartik_fs_io/fs_io.dart';
 import 'package:tekartik_fs_test/fs_test.dart';
 import 'package:tekartik_fs_test/test_common.dart';
 import 'package:tekartik_platform/context.dart';
 import 'package:tekartik_platform_io/context_io.dart';
-@TestOn('vm')
 import 'package:test/test.dart';
 
 class FileSystemTestContextIo extends FileSystemTestContext {
   @override
   final PlatformContext platform = platformContextIo;
-
-  //@override
-  //@deprecated
-  //String get outPath => join('.dart_tool', 'tekartik_fs_io', super.outPath);
 
   @override
   FileSystem fs = fileSystem; // Needed for initialization (supportsLink)
