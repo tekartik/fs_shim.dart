3641e13e9c5483975685de972fd4231eaba753e9
Alexandre Roux <alex@tekartik.com>
2023-01-08 13:10:13 +0100

    [fs_shim] fix tests

--- fs/test/test_common.dart
+++ fs/test/test_common.dart
@@ -11,6 +11,8 @@ import 'package:fs_shim/src/idb/idb_file_system.dart';
 import 'package:fs_shim/src/platform/platform.dart';
 import 'package:test/test.dart';
 
+import 'multiplatform/fs_test_common.dart';
+
 export 'dart:async';
 export 'dart:convert';
 
@@ -27,92 +29,4 @@ export 'package:fs_shim/utils/path.dart';
 export 'package:fs_shim/utils/read_write.dart';
 export 'package:test/test.dart';
 
-int _testId = 0;
-
-List<String> get testDescriptions => ['test${++_testId}'];
-
-// FileSystem context
-abstract class FileSystemTestContext {
-  PlatformContext? get platform;
-
-  // The file system used
-  FileSystem get fs;
-
-  // The path to use for testing
-  String get outPath => fs.path.joinAll(testDescriptions);
-
-  Future<Directory> prepare() async {
-    final dir = fs.directory(outPath);
-    try {
-      await dir.delete(recursive: true);
-    } on FileSystemException catch (e) {
-      //print(e);
-      try {
-        expect(e.status, FileSystemException.statusNotFound);
-      } catch (te) {
-        // devPrint('delete exception $e');
-        expect(e.status, FileSystemException.statusAccessError);
-      }
-    }
-    await dir.create(recursive: true);
-    return dir.absolute;
-  }
-}
-
-abstract class IdbFileSystemTestContext extends FileSystemTestContext {
-  @override
-  IdbFileSystem get fs;
-
-  @override
-  String toString() => 'IdbFsTestContext($fs)';
-}
-
-final MemoryFileSystemTestContext memoryFileSystemTestContext =
-    MemoryFileSystemTestContext();
-
-class MemoryFileSystemTestContext extends IdbFileSystemTestContext {
-  final FileSystemIdbOptions? options;
-  @override
-  final PlatformContext? platform = null;
-  @override
-  late final IdbFileSystem fs = () {
-    if (debugIdbShowLogs) {
-      print('Creating file system $hashCode');
-    }
-    // IdbFactoryLogger.debugMaxLogCount = devWarning(256);
-    var fs = newFileSystemMemory();
-    if (options != null) {
-      fs = fs.withIdbOptions(options: options!);
-    }
-    return fs as IdbFileSystem;
-  }();
-
-  MemoryFileSystemTestContext({this.options});
-}
-
-void devPrintJson(Map json) {
-  print(const JsonEncoder.withIndent('  ').convert(json));
-}
-
-bool isIoWindows(FileSystemTestContext ctx) {
-  return isIo(ctx) && (ctx.platform as PlatformContextIo).isIoWindows == true;
-}
-
-bool isIoMac(FileSystemTestContext ctx) {
-  return isIo(ctx) && (ctx.platform as PlatformContextIo).isIoMacOS == true;
-}
-
-bool isIoLinux(FileSystemTestContext ctx) {
-  return isIo(ctx) && (ctx.platform as PlatformContextIo).isIoLinux == true;
-}
-
-bool isIo(FileSystemTestContext ctx) {
-  return ctx.platform?.isIo == true;
-}
-
-String jsonPretty(dynamic json) {
-  if (json is String) {
-    json = jsonDecode(json);
-  }
-  return const JsonEncoder.withIndent('  ').convert(json);
-}
+export 'multiplatform/fs_test_common.dart';
--- fs_test/lib/test_common.dart
+++ fs_test/lib/test_common.dart
@@ -1,18 +1,5 @@
 library fs_shim.test.test_common;
 
-// ignore_for_file: implementation_imports
-// basically same as the io runner but with extra output
-import 'dart:convert';
-
-import 'package:fs_shim/fs_idb.dart';
-import 'package:fs_shim/fs_memory.dart';
-import 'package:fs_shim/src/idb/idb_file_system.dart';
-import 'package:fs_shim/src/platform/platform.dart'
-    show PlatformContext, PlatformContextIo;
-import 'package:test/test.dart';
-
-import 'src/import_common.dart';
-
 export 'dart:async';
 export 'dart:convert';
 
@@ -33,94 +20,5 @@ export 'package:fs_shim/utils/path.dart';
 export 'package:fs_shim/utils/read_write.dart';
 export 'package:test/test.dart';
 
+export 'fs_test_common.dart';
 export 'src/import_common.dart';
-
-/// FileSystem test context
-abstract class FileSystemTestContext {
-  PlatformContext? get platform;
-
-  // The file system used
-  FileSystem get fs;
-
-  /// Base path
-  String? basePath;
-
-  static int _id = 0;
-
-  Directory _prepareNewDirectory() => fs.directory(fs.path
-      .joinAll(<String>[if (basePath != null) basePath!, 'out', '${++_id}']));
-
-  Future<Directory> prepare() async {
-    var outPath = _prepareNewDirectory().path;
-    final dir = fs.directory(outPath);
-    try {
-      await dir.delete(recursive: true);
-    } on FileSystemException catch (e) {
-      expect(e.status, FileSystemException.statusNotFound);
-    }
-    await dir.create(recursive: true);
-    var abs = dir.absolute;
-    return abs;
-  }
-}
-
-typedef FileSystemTestContextIdb = IdbFileSystemTestContext;
-
-abstract class IdbFileSystemTestContext extends FileSystemTestContext {
-  @override
-  PlatformContext? platform;
-  @override
-  FileSystemIdb get fs;
-}
-
-abstract class FileSystemTestContextIdbWithOptions
-    extends IdbFileSystemTestContext {
-  final FileSystemIdbOptions options;
-
-  FileSystemTestContextIdbWithOptions({required this.options});
-}
-
-final MemoryFileSystemTestContext memoryFileSystemTestContext =
-    MemoryFileSystemTestContext();
-
-class MemoryFileSystemTestContext extends IdbFileSystemTestContext {
-  MemoryFileSystemTestContext();
-
-  @override
-  final IdbFileSystem fs = newFileSystemMemory() as IdbFileSystem;
-}
-
-class MemoryFileSystemTestContextWithOptions
-    extends FileSystemTestContextIdbWithOptions {
-  MemoryFileSystemTestContextWithOptions({required super.options});
-
-  @override
-  final IdbFileSystem fs = newFileSystemMemory() as IdbFileSystem;
-}
-
-void devPrintJson(Map json) {
-  print(const JsonEncoder.withIndent('  ').convert(json));
-}
-
-String jsonPretty(dynamic json) {
-  if (json is String) {
-    json = jsonDecode(json);
-  }
-  return const JsonEncoder.withIndent('  ').convert(json);
-}
-
-bool isIoWindows(FileSystemTestContext ctx) {
-  return isIo(ctx) && (ctx.platform as PlatformContextIo).isIoWindows == true;
-}
-
-bool isIoMac(FileSystemTestContext ctx) {
-  return isIo(ctx) && (ctx.platform as PlatformContextIo).isIoMacOS == true;
-}
-
-bool isIoLinux(FileSystemTestContext ctx) {
-  return isIo(ctx) && (ctx.platform as PlatformContextIo).isIoLinux == true;
-}
-
-bool isIo(FileSystemTestContext ctx) {
-  return ctx.platform?.isIo == true;
-}
