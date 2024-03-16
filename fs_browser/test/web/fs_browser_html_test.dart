e417e7271e23b53af0bb74ffa89c5a4a343e9c3b
Alexandre Roux <alex@tekartik.com>
2024-03-16 17:44:20 +0100

    test both html/web_interop implementation

---
+++ fs_browser/test/web/fs_browser_html_test.dart
@@ -0,0 +1,26 @@
+@TestOn('browser')
+library fs_shim_browser.fs_browser_test;
+
+//import 'package:tekartik_fs_browser/fs_browser.dart';
+import 'package:fs_shim/fs_html.dart';
+import 'package:fs_shim/src/idb/idb_file_system.dart';
+import 'package:tekartik_fs_test/fs_test.dart';
+import 'package:tekartik_fs_test/test_common.dart';
+
+class FileSystemTestContextIdbBrowser extends IdbFileSystemTestContext {
+  @override
+  IdbFileSystem rawFsIdb = fileSystemIdb
+      as IdbFileSystem; // Needed for initialization (supportsLink)
+  FileSystemTestContextIdbBrowser() {
+    platform = platformContextBrowser;
+  }
+}
+
+FileSystemTestContextIdbBrowser fileSystemTestContextIdbBrowser =
+    FileSystemTestContextIdbBrowser();
+
+void main() {
+  group('browser', () {
+    defineTests(fileSystemTestContextIdbBrowser);
+  });
+}
